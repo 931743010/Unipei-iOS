e4fbcda6418429ef4b6eb8f66c8960d6b97986a7
qinjun998 <qinjun998@sohu.com>
2015-12-24 16:01:52 +0800

    commit change

---
+++ Codebase/UnipeiApp/Pages/Lottery/ShowLotteryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowLotteryViewController.h
+//  DymIOSApp
+//
+//  Created by MacBook on 12/24/15.
+//  Copyright © 2015 Dong Yiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowLotteryViewController : UIViewController
+
+@end
