51622bed119c1ed485447a4e56c9df0a513e2a26
Daniel Dong <dymx101@hotmail.com>
2015-12-25 14:48:13 +0800

    choose model

--- Codebase/UnipeiApp/Pages/4S/UNP4sQueryVC.m
+++ Codebase/UnipeiApp/Pages/4S/UNP4sQueryVC.m
@@ -10,8 +10,14 @@
 #import "JPSubmitButton.h"
 #import "JPBorderedTextField.h"
 #import <Masonry/Masonry.h>
+#import "UNPCarModelChooseVM.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import "UNPChooseBrandVC.h"
+#import "JPSidePopVC.h"
 
-@interface UNP4sQueryVC ()
+@interface UNP4sQueryVC () {
+    UNPCarModelChooseVM     *_carModelChooseVM;
+}
 
 @property (weak, nonatomic) IBOutlet UIView *bgChooseModel;
 @property (weak, nonatomic) IBOutlet JPSubmitButton *btnChooseModel;
@@ -34,16 +40,22 @@
 }
 
 - (void)viewDidLoad {
+    @weakify(self)
     [super viewDidLoad];
     
     self.navigationItem.title = @"4S报价查询";
+    
+    _carModelChooseVM = [UNPCarModelChooseVM new];
     
     _bgChooseModel.layer.masksToBounds = _bgOE.layer.masksToBounds = _bgPart.layer.masksToBounds = YES;
     _bgChooseModel.layer.cornerRadius = _bgOE.layer.cornerRadius = _bgPart.layer.cornerRadius = 5;
     _bgChooseModel.layer.borderWidth = _bgOE.layer.borderWidth = _bgPart.layer.borderWidth = 0.5;
     _bgChooseModel.layer.borderColor = _bgOE.layer.borderColor = _bgPart.layer.borderColor = [UIColor colorWithWhite:0.8 alpha:1].CGColor;
     
+    _btnChooseModel.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    _btnChooseModel.titleLabel.textAlignment = NSTextAlignmentCenter;
     _btnChooseModel.style = kJPButtonWhite;
+    _btnChooseModel.verticalWhiteSpace = 20;
     _btnSubmit.style = kJPButtonOrange;
     
     _tfVinCode.backgroundColor = _tfOE.backgroundColor = _tfPart.backgroundColor = [UIColor clearColor];
@@ -56,6 +68,28 @@
     self.tableView.rowHeight = UITableViewAutomaticDimension;
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.tableView.contentInset = UIEdgeInsetsMake(10, 0, 0, 0);
+    
+    //通知
+    id observer = [[NSNotificationCenter defaultCenter] addObserverForName:NOTIFY_String_carModelSelected object:nil queue:nil usingBlock:^(NSNotification *note) {
+        @strongify(self)
+        if ([note.object isKindOfClass:[UNPCarModelChooseVM class]]) {
+            self->_carModelChooseVM = note.object;
+            [self->_btnChooseModel setTitle:[self->_carModelChooseVM fullName] forState:UIControlStateNormal];
+        }
+    }];
+    
+    [self.observerQueue addObject:observer];
+    
+    // 按钮事件
+    [[_btnChooseModel rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+        @strongify(self)
+        UNPChooseBrandVC *vc = [UNPChooseBrandVC newFromStoryboard];
+        vc.viewModel = [self->_carModelChooseVM copy];
+        UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+        
+        [[JPSidePopVC new] showVC:nc];
+        [self.view endEditing:YES];
+    }];
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
